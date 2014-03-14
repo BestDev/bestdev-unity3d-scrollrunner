9b6b7ded73ff399e7702e12f02599d8c08b40132
BestDev <bestdev78@gmail.com>
2014-03-14 18:59:31 +0900

    // 좌우 이동 및 점프 // 리지바디 물리 적용 // 함정 판넬 추가

--- Assets/Runner/Runner.cs
+++ Assets/Runner/Runner.cs
@@ -3,30 +3,118 @@ using System.Collections;
 
 public class Runner : MonoBehaviour
 {
-	public static int nScrollFlag = 0;	// 0 : 횡스크롤, 1 : 종스크롤
+	public static int nScrollFlag = 1;	// 0 : 횡스크롤, 1 : 종스크롤
 	public static float distanceTraveled;
+	public Vector3 chrSize;
+	public float runSpeed = 30;
+
+	public float acceleration = 30;
+	public Vector3 jumpVelocity;
+
+	public GUIText guitext;
+
+	private bool touchingTile;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
 	}
 	
 	// Update is called once per frame
 	void Update () 
 	{
+		// 캐릭터 사이즈 설정
+		Vector3 scale = new Vector3(
+			Random.Range(chrSize.x, chrSize.x),
+			Random.Range(chrSize.y, chrSize.y),
+			Random.Range(chrSize.z, chrSize.z));
+
+		transform.localScale = scale;
+
+		// 마우스 스크롤 스피드 업 / 다운
+		// if(Input.GetKeyUp(KeyCode.PageUp))
+		if(Input.GetAxis("Mouse ScrollWheel") > 0)
+		{
+			runSpeed += 10;
+			acceleration += 10;
+		}
+
+		// if(Input.GetKeyUp(KeyCode.mou.PageDown))
+		if(Input.GetAxis("Mouse ScrollWheel") < 0)
+		{
+			runSpeed -= 10;
+			acceleration -= 10;
+		}
+
+		//Debug.Log("RunSpeed : " + runSpeed);
+		//guitext.text = "RunSpeed : " + runSpeed;
+		guitext.text = "acceleration : " + acceleration;
+
+		// 페이지 업 / 다운 타일 난이도 설정 (1 ~ 10)
+		if(Input.GetKeyUp(KeyCode.PageUp))
+		{
+			if(TileManager.gapOfRandom < 10)
+			{
+				TileManager.gapOfRandom += 1;
+			}
+		}
+		else if(Input.GetKeyUp(KeyCode.PageDown))
+		{
+			if(TileManager.gapOfRandom > 1)
+			{
+				TileManager.gapOfRandom -= 1;
+			}
+		}
+
+		// 좌우 키 입력
+		float keySide = Input.GetAxis("Horizontal");
+		transform.Translate(Vector3.right * (runSpeed / 2) * Time.deltaTime * keySide);
+		
+		//Debug.Log("gapOfRandom : " + TileManager.gapOfRandom);
+
+		/*
 		if(nScrollFlag == 0)
 		{
 			// 횡스크롤
-			transform.Translate(5f * Time.deltaTime, 0f, 0f);
+			transform.Translate(runSpeed * Time.deltaTime, 0f, 0f);
 
 			distanceTraveled = transform.localPosition.x;
 		}
 		else
 		{
+		*/
 			// 종스크롤
-			transform.Translate(0f, 0f, 5f * Time.deltaTime);
-
-			distanceTraveled = transform.localPosition.z;
+			transform.Translate(0f, 0f, runSpeed * Time.deltaTime);
+		if(touchingTile && Input.GetButton("Jump"))
+		{
+			rigidbody.AddForce(jumpVelocity, ForceMode.VelocityChange);
+			touchingTile = false;
 		}
+
+		distanceTraveled = transform.localPosition.z;
+		/*
+		}
+		*/
+	}
+
+	void FixedUpdate()
+	{
+		if(touchingTile)
+		{
+			//rigidbody.AddForce(0f, 0f, acceleration, ForceMode.Acceleration);
+			//Debug.Log("뭐임?");
+		}
+	}
+
+	void OnCollisionEnter()
+	{
+		touchingTile = true;
+		//Debug.Log("true");
+	}
+
+	void OnCollisionExit()
+	{
+		touchingTile = false;
+		//Debug.Log("false");
 	}
 }
---
+++ Assets/Tile/TileManager.cs
@@ -0,0 +1,146 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TileManager : MonoBehaviour
+{
+	public Transform prefab;
+	public int numberOfObjects;
+	public float recycleOffset;
+	public Vector3 startPosition;
+	public Vector3 minSize, maxSize;
+	//public int minRnd = 1, maxRnd;
+	public static int gapOfRandom = 10;
+
+	public Material[] materials;
+	public PhysicMaterial[] physicMaterials;
+
+	private Vector3 nextPosition;
+	private Queue<Transform> objectQueue;
+
+	private float fQueuePosition;
+	private int nRnd;
+
+	// Use this for initialization
+	void Start ()
+	{
+		objectQueue = new Queue<Transform>(numberOfObjects);
+		for(int i = 0; i < numberOfObjects; i++)
+		{
+			objectQueue.Enqueue((Transform)Instantiate(prefab));
+		}
+
+		nextPosition = startPosition;
+
+		for(int i = 0; i < numberOfObjects; i++)
+		{
+			Recycle();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		/*
+		if(Runner.nScrollFlag == 0)
+		{
+			fQueuePosition = objectQueue.Peek().localPosition.x;
+		/*
+		}
+		else
+		{
+		*/
+			fQueuePosition = objectQueue.Peek().localPosition.z;
+		/*
+		}
+		*/
+
+		if(fQueuePosition + recycleOffset < Runner.distanceTraveled -10)
+		{
+			Recycle();
+		}
+	}
+
+	private void Recycle()
+	{
+		Vector3 scale = new Vector3(
+			Random.Range(minSize.x, maxSize.x),
+			Random.Range(minSize.y, maxSize.y),
+			Random.Range(minSize.z, maxSize.z));
+
+		Vector3 position = nextPosition;
+
+		/*
+		if(Runner.nScrollFlag == 0)
+		{
+			position.x += scale.x * 0.5f;
+			position.y += scale.y * 0.5f;
+		}
+		else
+		{
+		*/
+			position.z += scale.z * 0.5f;
+			position.y += scale.y * 0.5f;
+		/*
+		}
+		*/
+
+		Transform o = objectQueue.Dequeue();
+		o.localScale = scale;
+		o.localPosition = position;
+
+		int materialIndex = Random.Range(0, materials.Length);
+		o.renderer.material = materials[materialIndex];
+		o.collider.material = physicMaterials[materialIndex];
+
+		//nRnd = Random.Range(minRnd, maxRnd);
+		nRnd = Random.Range(1, 100);
+		//Debug.Log("Random : " + nRnd);
+
+		nRnd = nRnd % gapOfRandom;
+
+		//Debug.Log ("rnd % : " + nRnd);
+
+		if(nRnd == 0)
+		{
+			nRnd = 2;
+		}
+		/*else if(nRnd < 2)
+		{
+			nRnd = 3;
+		}*/
+		else
+		{
+			nRnd = 1;
+		}
+
+		//Debug.Log ("Result : " + nRnd);
+
+		/*
+		if(Runner.nScrollFlag == 0)
+		{
+			nextPosition.x += (scale.x * nRnd);
+		}
+		else
+		{
+		*/
+			nextPosition.z += (scale.z * nRnd);
+		/*
+		}
+		*/
+		/*
+		Transform o = objectQueue.Dequeue();
+		o.localPosition = nextPosition;
+		
+		if(Runner.nScrollFlag == 0)
+		{
+			nextPosition.x += o.localScale.x;
+		}
+		else
+		{
+			nextPosition.z += o.localScale.z;
+		}
+		*/
+
+		objectQueue.Enqueue(o);
+	}
+}
